b9395584d707ed929b01e3b4cc35421085a7846c
Pawel Godziuk <pawel.godziuk@gmail.com>
2017-05-15 16:06:42 +0200

    model Category, testing foreign_keys in tree

--- test/models/category_test.rb
+++ test/models/category_test.rb
@@ -1,7 +1,10 @@
 require 'test_helper'
 
 class CategoryTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  test "tree test if parent_id is added correctly" do
+    root = Category.create(name: "sport")
+    child1 = root.children.create(name: "rowery")
+    child2 = child1.children.create(name: "części i akcesoria")
+    assert_equal child1.id, child2.parent_id
+  end
 end
